61bd0a7d7aa6ac98534e1b706f7ac71b22d71005
Julien <julien@pop-os.localdomain>
2020-10-10 23:43:52 +0200

    Updates

---
+++ precache-manifest.22e3acfc0cc163a8e366669d23e64a9d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3155f22353fb488a2f93b5fc9b01bbe",
+    "url": "/react-squad-manager/index.html"
+  },
+  {
+    "revision": "25beecc3232b5ae2d6ac",
+    "url": "/react-squad-manager/static/css/main.31129739.chunk.css"
+  },
+  {
+    "revision": "8e00baa2ea8966e02f24",
+    "url": "/react-squad-manager/static/js/2.2c7aa530.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-squad-manager/static/js/2.2c7aa530.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25beecc3232b5ae2d6ac",
+    "url": "/react-squad-manager/static/js/main.b3d19d05.chunk.js"
+  },
+  {
+    "revision": "61853c204efabef35482",
+    "url": "/react-squad-manager/static/js/runtime-main.881eea54.js"
+  },
+  {
+    "revision": "b1b40c25bdcf0b6b7e99b178a5c95e06",
+    "url": "/react-squad-manager/static/media/jersey.b1b40c25.svg"
+  },
+  {
+    "revision": "f8e6342c3af8850b91253caf57547687",
+    "url": "/react-squad-manager/static/media/soccer-field.f8e6342c.svg"
+  }
+]);
